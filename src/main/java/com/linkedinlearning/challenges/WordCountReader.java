f7cefb2a980591c9da3acfc6abe95115f92c6b6e
Kimmeyy <126781970+Kimmeyy@users.noreply.github.com>
2023-03-31 12:27:44 +0000

    WordCountReader improved

--- src/main/java/com/linkedinlearning/challenges/WordCountReader.java
+++ src/main/java/com/linkedinlearning/challenges/WordCountReader.java
@@ -1,37 +1,51 @@
 package com.linkedinlearning.challenges;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.NoSuchFileException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
 
 public class WordCountReader {
 
-  private String content;
+  private List<String> content;
 
   void readFile(String path) throws IOException {
     try {
-      File file = new File(path);
-      FileInputStream fileStream = new FileInputStream(file);
-
-      fileStream.close();
-    } catch (FileNotFoundException e) {
+      Path p = Paths.get(path);
+      List<String> lines = Files.readAllLines(p);
+      this.content = lines;
+    } catch (NoSuchFileException e) {
       System.out.println("Die Datei existiert nicht:");
       e.printStackTrace();
-    }
+    } 
   }
 
   int getWords() {
-    String[] anzWords = content.split(" ");
-    return anzWords.length;
+    int anz = 0;
+    for (String string : content) {
+      String[] anzWords = string.split(" ");
+      anz += anzWords.length;
+    }
+    return anz;
   }
 
   int getCharacters() {
-    return content.length();
+    int anz = 0;
+    for (String string : content) {
+      anz += string.length();
+    }
+    return anz;
   }
 
   int getCharactersExcludingSpaces() {
-    String noSpaces = content.replaceAll(" ", "");
-    return noSpaces.length();
+    int anz = 0;
+    for (String string : content) {
+      String noSpaces = string.replaceAll(" ", "");
+      anz += noSpaces.length();
+    }
+    return anz;
+
   }
 }
--- src/test/java/com/linkedinlearning/challenges/WordCountReaderTest.java
+++ src/test/java/com/linkedinlearning/challenges/WordCountReaderTest.java
@@ -2,16 +2,30 @@ package com.linkedinlearning.challenges;
 
 import java.io.IOException;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class WordCountReaderTest {
-  
+
   @Test
   void wordCountReaderTest() throws IOException {
     WordCountReader wordCountReader = new WordCountReader();
     wordCountReader.readFile("src/test/resources/simple_words.txt");
-    System.out.println(wordCountReader.getWords());
-    System.out.println(wordCountReader.getCharacters());
-    System.out.println(wordCountReader.getCharactersExcludingSpaces());
+
+    int actual1 = wordCountReader.getWords();
+    int actual2 = wordCountReader.getCharacters();
+    int actual3 = wordCountReader.getCharactersExcludingSpaces();
+
+    System.out.println(actual1);
+    System.out.println(actual2);
+    System.out.println(actual3);
+
+    int expected1 = 18;
+    int expected2 = 86;
+    int expected3 = 70;
+
+    Assertions.assertEquals(actual1, expected1);
+    Assertions.assertEquals(actual2, expected2);
+    Assertions.assertEquals(actual3, expected3);
   }
 }
