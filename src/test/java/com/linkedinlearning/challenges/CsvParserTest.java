3e25c2bdb5bed537f3c199b8bb5b37e92818680d
Kimmeyy <126781970+Kimmeyy@users.noreply.github.com>
2023-04-05 12:35:42 +0000

    CsvParserTest

--- src/test/java/com/linkedinlearning/challenges/CsvParserTest.java
+++ src/test/java/com/linkedinlearning/challenges/CsvParserTest.java
@@ -11,6 +11,9 @@ public class CsvParserTest {
     CsvParser parser = new CsvParser();
     var erg = parser.parse("src/test/resources/turing.csv");
     System.out.println(erg.get(1983));
+    System.out.println(erg);
     Assertions.assertEquals("Richard Hamming", erg.get(1968));
+    Assertions.assertEquals(
+        "Allen Newell, Herbert A. Simon", erg.get(1975));
   }
 }
